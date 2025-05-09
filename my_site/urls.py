88435ba1693e4c5514c1ea4e34720155fc6a7371
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-09 12:33:15 +0300

    Изменен маршрут для приложения main_page в файле urls.py: теперь он начинается с "main_page/" вместо корневого пути.

--- my_site/urls.py
+++ my_site/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_page.urls')),
+    path("main_page/", include("main_page.urls")),
 ]
