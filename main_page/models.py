d52f61ebec6157cfd7bcf94a4ad7db658c3b798f
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-09 13:30:19 +0300

    Удалена модель AboutMe из файла models.py, так как она больше не используется в проекте.

--- main_page/models.py
+++ main_page/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 
-class AboutMe(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    city = models.CharField(max_length=100)
-    email = models.EmailField()
-    telegram = models.CharField(max_length=100)
-    about = models.TextField()
