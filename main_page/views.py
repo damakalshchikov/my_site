e838396bb814daf05bfdadcb2ecee2053125664e
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-09 13:03:22 +0300

    Удалено представление "О себе" и добавлено новое представление для главной страницы, которое использует шаблон main_page.html.

--- main_page/views.py
+++ main_page/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-from .models import AboutMe
 
-
-def about_me(request):
-    about_me_data = AboutMe.objects.first()
-    return render(request, "main_page/about_me.html", {"about_me": about_me_data})
+def main_page(request):
+    return render(request, "main_page/main_page.html")
