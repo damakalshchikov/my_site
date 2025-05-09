58cfaab8e3e18fa1fa2cd22336a9998d231f9673
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-09 12:49:49 +0300

    Изменено представление "О себе" для использования шаблона и добавлены данные из модели AboutMe. Создан новый шаблон about_me.html для отображения информации о пользователе.

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import AboutMe
 
 
 def about_me(request):
-    return HttpResponse("Hello, World!")
+    about_me_data = AboutMe.objects.first()
+    return render(request, "main_page/about_me.html", {"about_me": about_me_data})
