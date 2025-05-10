e286d12bbac712a4802b038048318004982c3b76
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-10 09:59:34 +0300

    Добавлены новые маршруты для тестовых представлений

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from . import views
 
 
+# От главной страницы идут ссылки на другие страницы
+# Блог будте отдельным приложением
+# Цели, об авторе, материал, который я посчитал полезным - это отдельные страницы, которые находятся в этом приложении
 urlpatterns = [
     path("", views.main_page, name="main_page"),
+    path("blog/", views.blog, name="blog"),
+    path("goals/", views.goals, name="goals"),
+    path("about/", views.about, name="about"),
+    path("resources/", views.resources, name="resources"),
 ]
