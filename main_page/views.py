a8b2560e4f53e39c7056bced2c985d968894ed37
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-10 09:59:06 +0300

    Добавлены тестовые представления. От них я буду отталкиваться, как создавать сайт

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def main_page(request):
-    return render(request, "main_page/main_page.html")
+    return render(request, "index.html")
+
+
+def blog(request):
+    return HttpResponse("Блог")
+
+
+def goals(request):
+    return HttpResponse("Цели")
+
+
+def about(request):
+    return HttpResponse("Об авторе")
+
+
+def resources(request):
+    return HttpResponse("Материал, который я посчитал полезным")
